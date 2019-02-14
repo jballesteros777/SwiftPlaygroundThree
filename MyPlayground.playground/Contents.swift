c4fb47a4ee156d659090bce1d81b48f984beb3a2
CM Student <cmstudent@UHWO-ACM-C-07.local>
2019-02-14 10:05:49 -1000

    Swift Playground #3 Arrays

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,9 +4,18 @@ import UIKit
 
 var str = "Hello, playground"
 
-var fruitNames : [String]
-fruitNames = ["Kiwis", "Apples", "Watermelons"]
-
+//Part 2
+var fruitNames: [String]
+fruitNames = ["Peaches", "Bananas", "Cantaloupe"]
+//assign fruitNames with 3 fruits
 print(fruitNames)
 
 print("I like to eat \(fruitNames)")
+
+for name in fruitNames {
+    print("I like to eat " + name)
+}
+
+print(fruitNames[1])
+print(fruitNames[0])
+
