49e832d30e254364d155c676062732c557a3663b
CM Student <cmstudent@UHWO-ACM-C-07.local>
2019-02-14 09:52:00 -1000

    Added Playground #3

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,3 +3,10 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var fruitNames : [String]
+fruitNames = ["Kiwis", "Apples", "Watermelons"]
+
+print(fruitNames)
+
+print("I like to eat \(fruitNames)")
