e875d8fc9430000196811b4ac5d7c6739bcb0b21
Jordan Ballesteros <pookele@gmail.com>
2019-02-17 14:19:55 -1000

    Assignment #4 Completed

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -22,10 +22,8 @@ print(fruitNames[0])
 //Part 8, Start of Assignment #4
 var myNumbers: [Int] = [4, 8, 16, 32, 64]
 
+let removed = myNumbers.remove(at: 2)
+
 for number in myNumbers {
     print("My favorite number is", number)
 }
-let removed = myNumbers.remove(at: 0)
-print(myNumbers)
-
-
